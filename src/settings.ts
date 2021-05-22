bba9d7339081c086d204af2eada449d7799af399
yuki_yano <yuki.ycino@gmail.com>
2021-05-22 23:07:09 +0900

    Refactor settings value

--- src/settings.ts
+++ src/settings.ts
@@ -1,2 +1,11 @@
 export const ZENO_DEFAULT_FZF_OPTIONS =
   Deno.env.get("ZENO_DEFAULT_FZF_OPTIONS") ?? "";
+
+export const HOME = Deno.env.get("HOME");
+export const SETTING_FILE = `${HOME}/.config/zeno/config.yml`;
+
+export const ZENO_GIT_CAT = Deno.env.get("ZENO_GIT_CAT") ?? "cat";
+export const ZENO_GIT_TREE = Deno.env.get("ZENO_GIT_TREE") ?? "tree";
+
+export const ZENO_DISABLE_BUILTIN_COMPLETION =
+  Deno.env.get("ZENO_DISABLE_BUILTIN_COMPLETION") == null ? false : true;
