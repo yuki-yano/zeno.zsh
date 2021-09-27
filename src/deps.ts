62dd3ae7db4c2558d474a60ccb8f255af78188e6
yuki_yano <yuki.ycino@gmail.com>
2021-09-27 11:42:27 +0900

    Fix use yargs version

--- src/deps.ts
+++ src/deps.ts
@@ -5,5 +5,5 @@ export { iter, readAllSync } from "https://deno.land/std@0.94.0/io/util.ts";
 // TODO: Use Deno.run https://deno.land/manual@v1.13.2/examples/subprocess
 export { exec, OutputMode } from "https://deno.land/x/exec@0.0.5/mod.ts";
 
-import argsParser from "https://deno.land/x/yargs_parser/deno.ts";
+import argsParser from "https://deno.land/x/yargs_parser@yargs-parser-v20.2.9-deno/deno.ts";
 export { argsParser };
