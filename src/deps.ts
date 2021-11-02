3e2f86f9d1e5074d6ead190dada5c24c47aa2b8b
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2021-11-02 10:47:18 +0900

    Update dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,7 +1,7 @@
-export { existsSync } from "https://deno.land/std@0.94.0/fs/exists.ts";
-export { parse as yamlParse } from "https://deno.land/std@0.94.0/encoding/yaml.ts";
-export { printf, sprintf } from "https://deno.land/std@0.94.0/fmt/printf.ts";
-export { iter } from "https://deno.land/std@0.94.0/io/util.ts";
+export { existsSync } from "https://deno.land/std@0.113.0/fs/exists.ts";
+export { parse as yamlParse } from "https://deno.land/std@0.113.0/encoding/yaml.ts";
+export { printf, sprintf } from "https://deno.land/std@0.113.0/fmt/printf.ts";
+export { iterateReader } from "https://deno.land/std@0.113.0/streams/conversion.ts";
 // TODO: Use Deno.run https://deno.land/manual@v1.13.2/examples/subprocess
 export { exec, OutputMode } from "https://deno.land/x/exec@0.0.5/mod.ts";
 
