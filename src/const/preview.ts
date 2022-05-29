c05bf4de1a7aba0595429a90ba0ee746c43286d6
Yoichi TAKAI <takai@cmscom.jp>
2022-05-29 18:32:40 +0900

    :bug: Fix preview command for 'git stash'

--- src/const/preview.ts
+++ src/const/preview.ts
@@ -10,4 +10,4 @@ export const GIT_LOG_PREVIEW = "git show --color=always {2}";
 export const GIT_BRANCH_LOG_TAG_REFLOG_PREVIEW =
   "[[ '{1}' == '[branch]' ]] && git log {2} --decorate --pretty='format:%C(yellow)%h %C(green)%cd %C(reset)%s %C(red)%d %C(cyan)[%an]' --date=iso --graph --color=always || [[ '{1}' == '[tag]' ]] && git log {2} --pretty='format:%C(yellow)%h %C(green)%cd %C(reset)%s %C(red)%d %C(cyan)[%an]' --date=iso --graph --color=always  || [[ '{1}' == '[commit]' ]] && git show --color=always {2} || [[ '{1}' == '[reflog]' ]] && git show --color=always {2}";
 
-export const GIT_STASH_PREVIEW = `git stash show -p "$(echo '{}' | ${ZENO_GIT_STASH_CUT})" | ${ZENO_GIT_CAT}`;
+export const GIT_STASH_PREVIEW = `${ZENO_GIT_STASH_CUT} | git stash show -p | ${ZENO_GIT_CAT}`;
