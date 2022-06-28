957970032dde68a60586ac74f6e613fe69bcc573
yuki_yano <yuki.ycino@gmail.com>
2022-06-28 12:23:09 +0900

    Improve stash callback

--- src/settings.ts
+++ src/settings.ts
@@ -9,7 +9,7 @@ export const ZENO_SOCK = Deno.env.get("ZENO_SOCK");
 export const ZENO_GIT_CAT = Deno.env.get("ZENO_GIT_CAT") ?? "cat";
 export const ZENO_GIT_TREE = Deno.env.get("ZENO_GIT_TREE") ?? "tree";
 export const ZENO_GIT_STASH_CUT = Deno.env.get("ZENO_GIT_STASH_CUT") ??
-  String.raw`cut -d' ' -f 1 | tr -d ':' | sed 's/[{}]/\\&/g'`;
+  String.raw`sed 's/:.*//; s/[{}]/\\&/g'`;
 
 export const ZENO_DISABLE_BUILTIN_COMPLETION =
   Deno.env.get("ZENO_DISABLE_BUILTIN_COMPLETION") == null ? false : true;
