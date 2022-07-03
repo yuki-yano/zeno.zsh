547dd15f7eefa15b83496eb5ac9d9bee978a4183
Milly <milly.ca@gmail.com>
2022-07-03 21:08:22 +0900

    Improve git completions
    - Added these sub-commands completions
      - `stash branch`: comlete git-branch-source
      - `stash push`: complete git-stats-source, multi
      - `stash show`: complete git-stash-source
      - `log`: complete branches or files
      - `tag --list`: complete git-log-source
      - `tag --delete`: complete git-tag-source, multi
      - `tag`: complete git-tag-source
      - `mv`: complete git-ls-files-source, multi
      - `rm`: complete git-ls-files-source, multi
      - `show`: complete git-log-source, multi
    - Fixed these sub-commands completions
      - `add`: fixed patterns, options
      - `diff`: fixed patterns, options
      - `commit`: fixed patterns, complete git-ls-files-source
      - `checkout`: fixed patterns
      - `reset`: fixed patterns
      - `switch`: fixed patterns
      - `restore`: fixed patterns
      - `rebase`: fixed patterns, complete git-branch-source
      - `merge`: fixed patterns, complete git-branch-source
      - `stash`: fixed patterns
    - Fixed source and preview formats

--- src/const/preview.ts
+++ src/const/preview.ts
@@ -1,13 +1,27 @@
 import { ZENO_GIT_CAT, ZENO_GIT_TREE } from "../settings.ts";
 
-export const GIT_STATUS_PREVIEW =
-  `[[ \\$(git diff -- {-1}) ]] && git diff --color=always -- {-1} || [[ \\$(git diff --cached -- {-1} ) ]] && git diff --cached --color=always -- {-1} || ${ZENO_GIT_CAT} {-1} 2>/dev/null || ${ZENO_GIT_TREE} {-1} 2>/dev/null`;
+const git_log_pretty_format =
+  "format:%C(yellow)%h %C(green)%cd %C(reset)%s %C(red)%d %C(cyan)[%an]";
+
+export const GIT_STATUS_PREVIEW = `
+  ! git diff --exit-code --color=always -- {-1}
+  || ! git diff --exit-code --cached --color=always -- {-1}
+  || ${ZENO_GIT_CAT} {-1} 2>/dev/null
+  || ${ZENO_GIT_TREE} {-1} 2>/dev/null
+`.trim().replaceAll(/\n\s*/g, "");
 
 export const GIT_LS_FILES_PREVIEW = `${ZENO_GIT_CAT} {}`;
 
 export const GIT_LOG_PREVIEW = "git show --color=always {2}";
 
-export const GIT_BRANCH_LOG_TAG_REFLOG_PREVIEW =
-  "[[ '{1}' == '[branch]' ]] && git log {2} --decorate --pretty='format:%C(yellow)%h %C(green)%cd %C(reset)%s %C(red)%d %C(cyan)[%an]' --date=iso --graph --color=always || [[ '{1}' == '[tag]' ]] && git log {2} --pretty='format:%C(yellow)%h %C(green)%cd %C(reset)%s %C(red)%d %C(cyan)[%an]' --date=iso --graph --color=always  || [[ '{1}' == '[commit]' ]] && git show --color=always {2} || [[ '{1}' == '[reflog]' ]] && git show --color=always {2}";
+export const GIT_BRANCH_LOG_TAG_REFLOG_PREVIEW = `
+  if [[ {1} == '[branch]' ]]; then
+    git log {2} --decorate --pretty='${git_log_pretty_format}' --date=iso --graph --color=always
+  elif [[ {1} == '[tag]' ]]; then
+    git log {2} --pretty='${git_log_pretty_format}' --date=iso --graph --color=always
+  elif [[ {1} == '[commit]' || {1} == '[reflog]' ]]; then
+    git show --color=always {2}
+  fi
+`.trim().replaceAll(/\n\s*/g, ";");
 
 export const GIT_STASH_PREVIEW = "git show --color=always {1}";
