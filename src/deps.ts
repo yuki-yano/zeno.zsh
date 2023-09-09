7adedd145bfdbd228cdfb687f21f514588944a84
GitHub <noreply@github.com>
2023-09-09 00:17:52 +0000

    chore(deps): update deno dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,8 +1,8 @@
-export { existsSync } from "https://deno.land/std@0.176.0/fs/exists.ts";
-export { parse as yamlParse } from "https://deno.land/std@0.176.0/encoding/yaml.ts";
-export { printf, sprintf } from "https://deno.land/std@0.176.0/fmt/printf.ts";
-export { iterateReader } from "https://deno.land/std@0.176.0/streams/iterate_reader.ts";
-export * as path from "https://deno.land/std@0.176.0/path/mod.ts";
+export { existsSync } from "https://deno.land/std@0.201.0/fs/exists.ts";
+export { parse as yamlParse } from "https://deno.land/std@0.201.0/encoding/yaml.ts";
+export { printf, sprintf } from "https://deno.land/std@0.201.0/fmt/printf.ts";
+export { iterateReader } from "https://deno.land/std@0.201.0/streams/iterate_reader.ts";
+export * as path from "https://deno.land/std@0.201.0/path/mod.ts";
 
 // TODO: Use Deno.run https://deno.land/manual@v1.13.2/examples/subprocess
 export { exec, OutputMode } from "https://deno.land/x/exec@0.0.5/mod.ts";
@@ -14,5 +14,5 @@ export type {
   Options as ArgParserOptions,
 } from "https://deno.land/x/yargs_parser@yargs-parser-v21.1.1-deno/lib/yargs-parser-types.ts";
 
-import xdg from "https://deno.land/x/xdg@v10.5.1/src/mod.deno.ts";
+import xdg from "https://deno.land/x/xdg@v10.6.0/src/mod.deno.ts";
 export { xdg };
