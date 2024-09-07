107cd16357ff7681dbd09e9897083f30605969e4
GitHub <noreply@github.com>
2024-09-07 00:23:28 +0000

    chore(deps): update deno dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,8 +1,8 @@
-export { existsSync } from "https://deno.land/std@0.201.0/fs/exists.ts";
-export { parse as yamlParse } from "https://deno.land/std@0.201.0/yaml/mod.ts";
-export { printf, sprintf } from "https://deno.land/std@0.201.0/fmt/printf.ts";
-export { iterateReader } from "https://deno.land/std@0.201.0/streams/iterate_reader.ts";
-export * as path from "https://deno.land/std@0.201.0/path/mod.ts";
+export { existsSync } from "https://deno.land/std@0.224.0/fs/exists.ts";
+export { parse as yamlParse } from "https://deno.land/std@0.224.0/yaml/mod.ts";
+export { printf, sprintf } from "https://deno.land/std@0.224.0/fmt/printf.ts";
+export { iterateReader } from "https://deno.land/std@0.224.0/streams/iterate_reader.ts";
+export * as path from "https://deno.land/std@0.224.0/path/mod.ts";
 
 // TODO: Use Deno.run https://deno.land/manual@v1.13.2/examples/subprocess
 export { exec, OutputMode } from "https://deno.land/x/exec@0.0.5/mod.ts";
--- test/deps.ts
+++ test/deps.ts
@@ -1,9 +1,9 @@
-export * as path from "https://deno.land/std@0.201.0/path/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.201.0/testing/asserts.ts";
+export * as path from "https://deno.land/std@0.224.0/path/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.224.0/testing/asserts.ts";
 export {
   afterEach,
   beforeAll,
   beforeEach,
   describe,
   it,
-} from "https://deno.land/std@0.201.0/testing/bdd.ts";
+} from "https://deno.land/std@0.224.0/testing/bdd.ts";
