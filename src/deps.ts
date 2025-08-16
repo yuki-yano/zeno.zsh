1889390ffcc0b46eb24aa02b7ec81ea8dddd5e5c
GitHub <noreply@github.com>
2025-08-16 00:01:12 +0000

    chore(deps): update deno dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,8 +1,8 @@
-export { exists } from "jsr:@std/fs@1.0.18/exists";
-export { parse as yamlParse } from "jsr:@std/yaml@1.0.8";
+export { exists } from "jsr:@std/fs@1.0.19/exists";
+export { parse as yamlParse } from "jsr:@std/yaml@1.0.9";
 export { printf, sprintf } from "jsr:@std/fmt@1.0.8/printf";
 export { iterateReader } from "jsr:@std/io@0.225.2/iterate-reader";
-export * as path from "jsr:@std/path@1.1.0";
+export * as path from "jsr:@std/path@1.1.2";
 
 import argsParser from "https://deno.land/x/yargs_parser@yargs-parser-v21.1.1-deno/deno.ts";
 export { argsParser };
--- test/deps.ts
+++ test/deps.ts
@@ -1,15 +1,15 @@
-export * as path from "jsr:@std/path@1.1.0";
+export * as path from "jsr:@std/path@1.1.2";
 export {
   assert,
   assertEquals,
   assertExists,
   assertStringIncludes,
   assertThrows,
-} from "jsr:@std/assert@1.0.13";
+} from "jsr:@std/assert@1.0.14";
 export {
   afterEach,
   beforeAll,
   beforeEach,
   describe,
   it,
-} from "jsr:@std/testing@1.0.14/bdd";
+} from "jsr:@std/testing@1.0.15/bdd";
