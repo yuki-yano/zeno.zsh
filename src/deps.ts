840f380444a97dfb65b53b56723b79790c6c2538
yuki_yano <yuki.ycino@gmail.com>
2025-10-22 19:09:05 +0900

    Switch deps to JSR modules
    - replace xdg dependency with jsr:@404wolf/xdg-portable
    - switch yargs-parser import to npm: scheme
    - widen publish.include so config deps are published

--- src/deps.ts
+++ src/deps.ts
@@ -1,17 +1,20 @@
+// deno-lint-ignore no-import-prefix
+import * as path from "jsr:@std/path@1.1.0";
+
 export { exists } from "jsr:@std/fs@1.0.18/exists";
 export { parse as yamlParse } from "jsr:@std/yaml@1.0.8";
 export { printf, sprintf } from "jsr:@std/fmt@1.0.8/printf";
 export { iterateReader } from "jsr:@std/io@0.225.2/iterate-reader";
-export * as path from "jsr:@std/path@1.1.0";
+export { path };
 
 // deno-lint-ignore no-import-prefix
-import argsParser from "https://deno.land/x/yargs_parser@yargs-parser-v21.1.1-deno/deno.ts";
+import argsParser from "npm:yargs-parser@21.1.1";
 export { argsParser };
 export type {
   Arguments as ArgParserArguments,
   Options as ArgParserOptions,
-} from "https://deno.land/x/yargs_parser@yargs-parser-v21.1.1-deno/lib/yargs-parser-types.ts";
+} from "npm:yargs-parser@21.1.1";
 
 // deno-lint-ignore no-import-prefix
-import xdg from "https://deno.land/x/xdg@v10.6.0/src/mod.deno.ts";
+import xdg from "jsr:@404wolf/xdg-portable@0.1.0";
 export { xdg };
