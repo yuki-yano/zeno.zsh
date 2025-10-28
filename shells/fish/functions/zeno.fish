ea4fad157e5c9df736ef8466415385c9dc99b0fe
yuki_yano <yuki.ycino@gmail.com>
2025-10-28 16:41:30 +0900

    Add --allow-net permission

--- shells/fish/functions/zeno.fish
+++ shells/fish/functions/zeno.fish
@@ -1,5 +1,5 @@
 function zeno
     command deno run --unstable-byonm --no-check \
-        --allow-env --allow-read --allow-run --allow-write \
+        --allow-env --allow-read --allow-run --allow-write --allow-net \
         -- "$ZENO_ROOT/src/cli.ts" $argv
 end
