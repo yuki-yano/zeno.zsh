e781f2a56e22b040b321a1e26abcff0d9ea158ed
GitHub <noreply@github.com>
2025-11-15 00:01:22 +0000

    chore(deps): update deno dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,19 +1,19 @@
 // deno-lint-ignore no-import-prefix
-import * as path from "jsr:@std/path@1.1.0";
+import * as path from "jsr:@std/path@1.1.2";
 
-export { exists } from "jsr:@std/fs@1.0.18/exists";
-export { parse as yamlParse } from "jsr:@std/yaml@1.0.8";
+export { exists } from "jsr:@std/fs@1.0.19/exists";
+export { parse as yamlParse } from "jsr:@std/yaml@1.0.10";
 export { printf, sprintf } from "jsr:@std/fmt@1.0.8/printf";
 export { iterateReader } from "jsr:@std/io@0.225.2/iterate-reader";
 export { path };
 
 // deno-lint-ignore no-import-prefix
-import argsParser from "npm:yargs-parser@21.1.1";
+import argsParser from "npm:yargs-parser@22.0.0";
 export { argsParser };
 export type {
   Arguments as ArgParserArguments,
   Options as ArgParserOptions,
-} from "npm:yargs-parser@21.1.1";
+} from "npm:yargs-parser@22.0.0";
 
 // deno-lint-ignore no-import-prefix
 import xdg from "jsr:@404wolf/xdg-portable@0.1.0";
