098d2e608a276e37e3adfad21922d33163243876
GitHub <noreply@github.com>
2026-03-28 00:04:23 +0000

    chore(deps): update deno dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,24 +1,24 @@
 // deno-lint-ignore no-import-prefix
-import * as path from "jsr:@std/path@1.1.0";
+import * as path from "jsr:@std/path@1.1.4";
 
-export { exists } from "jsr:@std/fs@1.0.18/exists";
-export { parse as yamlParse } from "jsr:@std/yaml@1.0.8";
-export { printf, sprintf } from "jsr:@std/fmt@1.0.8/printf";
-export { iterateReader } from "jsr:@std/io@0.225.2/iterate-reader";
+export { exists } from "jsr:@std/fs@1.0.23/exists";
+export { parse as yamlParse } from "jsr:@std/yaml@1.0.12";
+export { printf, sprintf } from "jsr:@std/fmt@1.0.9/printf";
+export { iterateReader } from "jsr:@std/io@0.225.3/iterate-reader";
 export { path };
 
 // deno-lint-ignore no-import-prefix
-import argsParser from "npm:yargs-parser@21.1.1";
+import argsParser from "npm:yargs-parser@22.0.0";
 export { argsParser };
 export type {
   Arguments as ArgParserArguments,
   Options as ArgParserOptions,
-} from "npm:yargs-parser@21.1.1";
+} from "npm:yargs-parser@22.0.0";
 
 // deno-lint-ignore no-import-prefix
 import xdg from "jsr:@404wolf/xdg-portable@0.1.0";
 export { xdg };
 
-export { Database } from "jsr:@db/sqlite@0.12.0";
-export type { BindParameters } from "jsr:@db/sqlite@0.12.0";
+export { Database } from "jsr:@db/sqlite@0.13.0";
+export type { BindParameters } from "jsr:@db/sqlite@0.13.0";
 export { ulid } from "jsr:@std/ulid@1.0.0";
